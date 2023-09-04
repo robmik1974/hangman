81f6e050cbb7e98ad5841430089b98d2e6127690
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-04 08:32:22 +0200

    keeps track of players lives

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
+import hangman_art
 
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
+
+lives = 6
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -11,13 +13,25 @@ display = list()
 for _ in range(len(chosen_word)):
     display.append("_")
 
-while '_' in display:
+end_of_the_game = False
+
+while not end_of_the_game:
 
     guess = input("Guess a letter: ").lower()
-    for index, letter in enumerate(chosen_word):
-        if guess == letter:
-            display[index] = letter
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word):
+            if guess == letter:
+                display[index] = letter
+    else:
+        lives -= 1
+        if lives == 0:
+            end_of_the_game = True
 
-    print(display)
-else:
-    print("You win!")
+    print(" ".join(display))
+    print(hangman_art.stages[lives])
+
+    if end_of_the_game:
+        print("You lose")
+    elif "_" not in display:
+        end_of_the_game = True
+        print("You win!")
